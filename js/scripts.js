e7b1d32980277c60af21b67de3246481015ef8f4
agatakolohe <agatakolohe@gmail.com>
2020-09-12 16:44:57 -0700

    make rows/cols for all vocab words, create display none for all df, all clicks color, toggle functions for all words

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,82 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $("#java-showing").toggle();
-    $("#java-hidden").toggle();
-    $("#op-showing").toggle();
-    $("#op-hidden").toggle();
+    $("#showing").toggle();
+    $("#hidden").toggle();
+  });
+  $(".click").click(function() {
+    $("#o-showing").toggle();
+    $("#o-hidden").toggle();
+  });
+  $(".v-click").click(function() {
+    $("#v-showing").toggle();
+    $("#v-hidden").toggle();
+  });
+  $(".vc-click").click(function() {
+    $("#vc-showing").toggle();
+    $("#vc-hidden").toggle();
+  });
+  $(".f-click").click(function() {
+    $("#f-showing").toggle();
+    $("#f-hidden").toggle();
+  });
+  $(".m-click").click(function() {
+    $("#m-showing").toggle();
+    $("#m-hidden").toggle();
+  });
+  $(".a-click").click(function() {
+    $("#a-showing").toggle();
+    $("#a-hidden").toggle();
+  });
+  $(".p-click").click(function() {
+    $("#p-showing").toggle();
+    $("#p-hidden").toggle();
+  });
+  $(".r-click").click(function() {
+    $("#r-showing").toggle();
+    $("#r-hidden").toggle();
+  });
+  $(".cm-click").click(function() {
+    $("#cm-showing").toggle();
+    $("#cm-hidden").toggle();
+  });
+  $(".s-click").click(function() {
+    $("#s-showing").toggle();
+    $("#s-hidden").toggle();
+  });
+  $(".b-click").click(function() {
+    $("#b-showing").toggle();
+    $("#b-hidden").toggle();
+  });
+  $(".ud-click").click(function() {
+    $("#ud-showing").toggle();
+    $("#ud-hidden").toggle();
+  });
+  $(".nan-click").click(function() {
+    $("#nan-showing").toggle();
+    $("#nan-hidden").toggle();
+  });
+  $(".n-click").click(function() {
+    $("#n-showing").toggle();
+    $("#n-hidden").toggle();
+  });
+  $(".al-click").click(function() {
+    $("#al-showing").toggle();
+    $("#al-hidden").toggle();
+  });
+  $(".c-click").click(function() {
+    $("#c-showing").toggle();
+    $("#c-hidden").toggle();
+  });
+  $(".jq-click").click(function() {
+    $("#jq-showing").toggle();
+    $("#jq-hidden").toggle();
+  });
+  $(".at-click").click(function() {
+    $("#at-showing").toggle();
+    $("#at-hidden").toggle();
+  });
+  $(".fu-click").click(function() {
+    $("#fu-showing").toggle();
+    $("#fu-hidden").toggle();
   });
 });
