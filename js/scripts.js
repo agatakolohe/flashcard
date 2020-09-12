b7105ebd9a4eed3ccac0f1d8e59a4cb8784d8f3a
agatakolohe <agatakolohe@gmail.com>
2020-09-11 23:15:11 -0700

    make 2 rows for javascript and operator, hide and toggle both, need to change clickable for operator

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#java-showing").toggle();
+    $("#java-hidden").toggle();
+    $("#op-showing").toggle();
+    $("#op-hidden").toggle();
+  });
+});
